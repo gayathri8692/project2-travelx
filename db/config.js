cd28ea27c8aaeed3fa3cef408c9a8e8e28e164e1
gayathri <gayathri.07310@gmail.com>
2017-05-07 23:15:58 -0400

    pushing to heroku

--- db/config.js
+++ db/config.js
@@ -1,18 +1,17 @@
 const pgp = require('pg-promise')();
 
 let db;
+
 if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
   db = pgp({
     database: 'travelapp',
     port: 5432,
     host: 'localhost',
   });
-} else if (process.env,NODE_ENV === 'production') {
-  db = pgp({
-    database: 'travelapp',
-    port: 5432,
-    host:'localhost',
-  });
+} else if (process.env.NODE_ENV === 'production') {
+  db = pgp(process.env.DATABASE_URL);
 }
+
+module.exports = db;
 
 module.exports = db;
